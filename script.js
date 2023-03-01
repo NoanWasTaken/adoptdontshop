b454cc5da98d4522d1aca514e393d8afab483ea6
Noan <delatouchenoan@gmail.com>
2023-03-01 10:56:48 +0100

    updating all the files i made previously

---
+++ script.js
@@ -0,0 +1,12 @@
+var title_container = document.getElementById("title_container");
+var register_btn = document.getElementById("register_btn");
+var register_form = document.getElementById("register_form");
+var submit_button = document.getElementById("submit_button");
+var form_container_one = document.getElementById("form_container_one");
+var button_container = document.getElementById("button_container");
+
+function registerAnimation() {
+    register_form.style.display = "block";
+    register_btn.style.display = "none";
+};
+
