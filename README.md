963c8b421ddba6e6589a2eb996e0809769387eb5
Noan <delatouchenoan@gmail.com>
2023-03-02 23:19:46 +0100

    Site ready
    the site is considered as finished (not functionnal) but it doesn't mean that i won't update it further

---
+++ README.md
@@ -0,0 +1,6 @@
+Informations concernant le site :
+La plupart des transitions sont contenues dans script.js
+
+Le responsive est a partir de 360 pixels (taille mobile plus fréquente) et 1366px (taille laptop de référence)
+
+La plupart du travail concernant le site réside dans le JS (une première pour le groupe), on espère que tu valoriseras un peu ce point :)
--- script.js
+++ script.js
@@ -8,8 +8,21 @@ var button_container = document.getElementById("button_container");
 let dog_id = [["dog1.png", "Noopie"], ["dog2.png", "Max"], ["dog3.png", "Lily"]]
 
 function registerAnimation() {
-    register_form.style.display = "block";
-    register_btn.style.display = "none";
+    register_form.style.transition = " 1.5s"
+   setTimeout(() => {
+    register_form.style.backgroundColor = "#fff"
+    register_form.style.backgroundColor = "#fff"
+
+   }, 1200); 
+    setTimeout(() => {
+        register_form.style.display = "block";
+    }, 1000);
+    register_btn.style.transition = "ease-in 0.5s";
+    register_btn.style.backgroundColor = "#9796DE";
+    register_btn.style.color = "#9796DE";
+    setTimeout(() => {
+        register_btn.style.display = "none";
+    }, 1000);
 };
 
 var dog_image = document.getElementById("dog_image");
@@ -33,4 +46,64 @@ dislike_btn.onclick = function () {
     var randint = Math.floor(Math.random() * 3);
     dog_image.src = "images/" + dog_id[randint][0];
     dog_name.innerHTML = dog_id[randint][1];
+}
+var isEnabled1 = false
+var asb1 = document.getElementById("asb1");
+asb1.onclick = function () {
+    if (isEnabled1 == true) {
+        asb1.style.transition = "0.3s all";
+        asb1.style.backgroundColor = "#fff";
+        isEnabled1 = false;
+    }
+    else {
+        asb1.style.transition = "0.3s all";
+        asb1.style.backgroundColor = "#9796DE";
+        isEnabled1 = true;
+    }
+}
+
+
+var isEnabled2 = false
+var asb2 = document.getElementById("asb2");
+asb2.onclick = function () {
+    if (isEnabled2 == true) {
+        asb2.style.transition = "0.3s all";
+        asb2.style.backgroundColor = "#fff";
+        isEnabled2 = false;
+    }
+    else {
+        asb2.style.transition = "0.3s all";
+        asb2.style.backgroundColor = "#9796DE";
+        isEnabled2 = true;
+    }
+}
+
+var isEnabled3 = false
+var asb3 = document.getElementById("asb3");
+asb3.onclick = function () {
+    if (isEnabled3 == true) {
+        asb3.style.transition = "0.3s all";
+        asb3.style.backgroundColor = "#fff";
+        isEnabled3 = false;
+    }
+    else {
+        asb3.style.transition = "0.3s all";
+        asb3.style.backgroundColor = "#9796DE";
+        isEnabled3 = true;
+    }
+}
+
+var isEnabled4 = false
+var asb4 = document.getElementById("asb4");
+asb4.onclick = function () {
+    if (isEnabled4 == true) {
+        asb4.style.transition = "0.3s all";
+        asb4.style.backgroundColor = "#fff";
+        isEnabled4 = false;
+    }
+    else {
+        asb4.style.transition = "0.3s all";
+        asb4.style.backgroundColor = "#9796DE";
+        isEnabled4 = true;
+    }
 }
